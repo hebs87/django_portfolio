e3244796f9c65c8d212b97803851bb5838baed02
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-02-23 21:58:12 +0000

    Created all_blogs view and displayed DB contents in all_blogs.html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .models import Blog
 
 
 # Create your views here.
 def all_blogs(request):
-    return render(request, 'all_blogs.html')
+    """
+    A view that gets the latest 5 posts in the
+    Blog table, orders them by newest first,
+    renders the all_blogs.html template and
+    injects the blogs into it
+    """
+    blogs = Blog.objects.order_by('-pub_date')[:5]
+
+    context = {
+        'blogs': blogs
+    }
+
+    return render(request, 'all_blogs.html', context)
--- portfolio/views.py
+++ portfolio/views.py
@@ -4,7 +4,11 @@ from .models import Project
 
 # Create your views here.
 def home(request):
-
+    """
+    A view that gets all the objects in the
+    Project table, renders the home.html
+    template and injects the projects into it
+    """
     projects = Project.objects.all()
 
     context = {
