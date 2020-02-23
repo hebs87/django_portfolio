ab15894e5ff2a4eb03c6ac1305ed4c11a7f71ad8
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-02-23 09:57:15 +0000

    Added Project model and registered on admin site

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Project(models.Model):
+    """
+    Stores information about user's projects/jobs
+    that they worked on
+    """
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    # Images will be uploaded to media/portfolio/images
+    image = models.ImageField(upload_to='portfolio/images/')
+    # blank=True makes this field optional
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return "You've added {} to your portfolio!".format(self.title)
